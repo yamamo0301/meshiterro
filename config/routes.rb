fd18fb0672671bee20046b1d071e19a8582d869c
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-24 14:15:47 +0000

    [Finish]5-19

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 
