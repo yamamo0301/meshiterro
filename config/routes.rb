9154505889e2362b12d29ae276498daed70a7c46
yamamo0301 <101253886+yamamo0301@users.noreply.github.com>
2022-05-24 13:18:26 +0000

    [Finish]5-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 
   devise_for :users
   root to: 'homes#top'
